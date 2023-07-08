6c293477e49a99b207ef50acadcd30a2228a9bec
risgrynsgrot <kingminiman@gmail.com>
2023-07-08 21:42:25 +0200

    added golfer

--- project/Scripts/Clapper.gd
+++ project/Scripts/Clapper.gd
@@ -10,15 +10,20 @@ var move_speed: Vector2
 var rotation_speed: float
 
 var is_active = true
+var last_position: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = get_viewport_rect().size / 2
+	position = get_viewport_rect().get_center()
 	position.x -= get_viewport_rect().size.x / 6
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	right_hand.global_position = get_global_mouse_position()
+	if last_position != right_hand.position:
+		right_hand.rotation = (last_position - right_hand.position).angle()
+	last_position = right_hand.position
+
 
 
 func _on_left_hand_collider_area_entered(area:Area2D):
