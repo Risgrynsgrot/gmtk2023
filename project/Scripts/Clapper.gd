db04db3e1c10efe81c7493fb757c95f3819b7b75
risgrynsgrot <kingminiman@gmail.com>
2023-07-08 14:22:01 +0200

    Changed project resolution

--- project/Scripts/Clapper.gd
+++ project/Scripts/Clapper.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@export var hand_in: Marker2D
-@export var hand_out: Marker2D
-
 @export var right_hand: Node2D
 
 @export var minigame: Node
@@ -18,9 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	right_hand.position = get_global_mouse_position()
 	if Input.is_action_just_pressed("left_click"):
-		right_hand.position = hand_in.position
 		minigame.emit_signal("on_clapped")
 
-	if Input.is_action_just_released("left_click"):
-		right_hand.position = hand_out.position
